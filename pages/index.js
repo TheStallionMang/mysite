fa72e21265b02c43e952bc4ac6513d7bf32b9cff
Christian Barroso <barrosochristian3@gmail.com>
2023-11-29 10:34:14 -0800

    stylistic changes and responsive

--- pages/index.js
+++ pages/index.js
@@ -8,42 +8,42 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <Layout>
-      <div className="container py-20 content-center title">
+      <div className="py-20 content-center title">
         <h1 className="text-5xl text-center underline">BARROSO</h1>
         <h3 className="text-3xl text-center">Web Development & Design</h3>
       </div>
-      <div className="container py-20 content-center grid grid-cols-2 gap-4 portfolioContainer">
+      <div className="py-20 content-center grid grid-cols-2 gap-4 portfolioContainer">
         <div className="portfolioColDiv">
-          <h1 className="text-5xl text-right underline">Portfolio:</h1>
+          <h1 className="text-4xl sm:text-5xl text-right underline">Portfolio:</h1>
         </div>
         <div className="portfolioList">
-          <p className="text-xl m-auto text-left">
+          <p className="sm:text-xl m-auto text-left">
             <Link
-              className="hover:underline hover:text-amber-500 m-auto"
+              className="m-auto p-2 link"
               href={"https://food-order-ecru.vercel.app/"}
             >
               Food order app
             </Link>
           </p>
-          <p className="text-xl text-left">
+          <p className="sm:text-xl text-left">
             <Link
-              className="hover:underline hover:text-amber-500"
+              className="m-auto p-2 link"
               href={"https://simple-posting-app.vercel.app/"}
             >
               Posting app
             </Link>
           </p>
-          <p className="text-xl text-left">
+          <p className="sm:text-xl text-left">
             <Link
-              className="hover:underline hover:text-amber-500"
+              className="m-auto p-2 link"
               href={"https://mad-cafe.vercel.app/"}
             >
               Mock local cafe site
             </Link>
           </p>
-          <p className="text-xl text-left">
+          <p className="sm:text-xl text-left">
             <Link
-              className="hover:underline hover:text-amber-500"
+              className="m-auto p-2 link"
               href={"https://cwa-sage.vercel.app/"}
             >
               Another mock cafe site
